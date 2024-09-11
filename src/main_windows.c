1d00ea86ba26fb5b9834657bb35fb379b3652ed1
Dawson Frakes <dawsonmfrakes@gmail.com>
2024-09-11 14:54:14 -0400

    add user32

--- src/main_windows.c
+++ src/main_windows.c
@@ -1,15 +1,26 @@
 /* kernel32 */
-void ExitProcess(u32);
-
-/* user32 */
 typedef struct HINSTANCE__* HINSTANCE;
 
 HINSTANCE GetModuleHandleW(u16*);
+void ExitProcess(u32);
+
+/* user32 */
+typedef struct HDC__* HDC;
+typedef struct HWND__* HWND;
+typedef struct HMENU__* HMENU;
+typedef struct HICON__* HICON;
+typedef struct HBRUSH__* HBRUSH;
+typedef struct HCURSOR__* HCURSOR;
+typedef struct HMONITOR__* HMONITOR;
+
+s32 SetProcessDPIAware(void);
 
 static HINSTANCE platform_hinstance;
 
 void WinMainCRTStartup(void) {
     platform_hinstance = GetModuleHandleW(null);
+
+    SetProcessDPIAware();
 
     ExitProcess(0);
 }
