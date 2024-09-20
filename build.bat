6e7f29ff1195be910bcf69b48a9a916156d132e3
Dawson Frakes <dawsonmfrakes@gmail.com>
2024-09-19 21:33:09 -0400

    clear screen, modernly.

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 @echo off
 
 rem tcc -impdef {kernel32,user32,gdi32,...}.dll
-tcc -DWinMainCRTStartup=_start -nostdlib main.c -lkernel32 -luser32 -lgdi32 -lopengl32 -ldwmapi -lwinmm -Wl,-subsystem=windows
+tcc -DWinMainCRTStartup=_start -nostdlib main.c -lkernel32 -luser32 -lgdi32 -lopengl32 -ldwmapi -lwinmm -Wl,-subsystem=windows || goto :error
 
 if "%1"=="test" (
     where /q cl || call vcvars64.bat || goto :error
     cl -nologo -W4 -WX -Z7 -Oi -J -EHa- -GR- -GS- -Gs0x1000000^
      main.c kernel32.lib user32.lib gdi32.lib opengl32.lib dwmapi.lib winmm.lib^
      -link -incremental:no -nodefaultlib -subsystem:windows^
-     -stack:0x1000000,0x1000000 -heap:0,0
+     -stack:0x1000000,0x1000000 -heap:0,0 || goto :error
+) else if "%1"=="run" ( start main.exe
 ) else if "%1"=="clean" (
     del *.obj *.exe *.pdb 2>nul
 )
--- renderer_opengl.c
+++ renderer_opengl.c
@@ -1,9 +1,41 @@
 // 1.0
 #define GL_COLOR_BUFFER_BIT 0x00004000
+#define GL_COLOR 0x1800
+#define GL_DEPTH 0x1801
+#define GL_NEAREST 0x2600
 
 #define GL10_FUNCTIONS \
+    X(void, glEnable, u32) \
+    X(void, glDisable, u32) \
+    X(void, glGetIntegerv, u32, s32*) \
     X(void, glClearColor, float32, float32, float32, float32) \
     X(void, glClear, u32)
+
+// 3.0
+#define GL_RGBA16F 0x881A
+#define GL_DEPTH_COMPONENT32F 0x8CAC
+#define GL_COLOR_ATTACHMENT0 0x8CE0
+#define GL_DEPTH_ATTACHMENT 0x8D00
+#define GL_FRAMEBUFFER 0x8D40
+#define GL_RENDERBUFFER 0x8D41
+#define GL_FRAMEBUFFER_SRGB 0x8DB9
+
+#define GL30_FUNCTIONS \
+    X(void, glBindFramebuffer, u32, u32) \
+    X(void, glBindVertexArray, u32)
+
+// 3.2
+#define GL_MAX_COLOR_TEXTURE_SAMPLES 0x910E
+#define GL_MAX_DEPTH_TEXTURE_SAMPLES 0x910F
+
+// 4.5
+#define GL45_FUNCTIONS \
+    X(void, glCreateFramebuffers, u32, u32*) \
+    X(void, glNamedFramebufferRenderbuffer, u32, u32, u32, u32) \
+    X(void, glClearNamedFramebufferfv, u32, u32, s32, float32*) \
+    X(void, glBlitNamedFramebuffer, u32, u32, s32, s32, s32, s32, s32, s32, s32, s32, u32, u32) \
+    X(void, glCreateRenderbuffers, u32, u32*) \
+    X(void, glNamedRenderbufferStorageMultisample, u32, u32, u32, u32, u32)
 
 #if TARGET_OS_WINDOWS
 #define WGL_CONTEXT_MAJOR_VERSION_ARB 0x2091
@@ -18,6 +50,10 @@ GL10_FUNCTIONS
 #undef X
 
 static HGLRC opengl_ctx;
+#define X(RET, NAME, ...) static RET (*NAME)(__VA_ARGS__);
+GL30_FUNCTIONS
+GL45_FUNCTIONS
+#undef X
 
 static void opengl_platform_init(void) {
     PIXELFORMATDESCRIPTOR pfd = {0};
@@ -47,6 +83,11 @@ static void opengl_platform_init(void) {
     wglMakeCurrent(platform_hdc, opengl_ctx);
 
     wglDeleteContext(temp_ctx);
+
+#define X(RET, NAME, ...) NAME = cast(RET (*)(__VA_ARGS__)) wglGetProcAddress(#NAME);
+    GL30_FUNCTIONS
+    GL45_FUNCTIONS
+#undef X
 }
 
 static void opengl_platform_deinit(void) {
@@ -59,8 +100,16 @@ static void opengl_platform_present(void) {
 }
 #endif
 
+static u32 opengl_main_fbo;
+static u32 opengl_main_fbo_color0;
+static u32 opengl_main_fbo_depth;
+
 static void opengl_init(void) {
     opengl_platform_init();
+
+    glCreateFramebuffers(1, &opengl_main_fbo);
+    glCreateRenderbuffers(1, &opengl_main_fbo_color0);
+    glCreateRenderbuffers(1, &opengl_main_fbo_depth);
 }
 
 static void opengl_deinit(void) {
@@ -68,11 +117,42 @@ static void opengl_deinit(void) {
 }
 
 static void opengl_resize(void) {
+    s32 fbo_color_samples_max;
+    glGetIntegerv(GL_MAX_COLOR_TEXTURE_SAMPLES, &fbo_color_samples_max);
+    s32 fbo_depth_samples_max;
+    glGetIntegerv(GL_MAX_DEPTH_TEXTURE_SAMPLES, &fbo_depth_samples_max);
+    u32 fbo_samples = cast(u32) min(fbo_color_samples_max, fbo_depth_samples_max);
 
+    if (platform_screen_width > 0 && platform_screen_height > 0) {
+        glNamedRenderbufferStorageMultisample(opengl_main_fbo_color0, fbo_samples,
+            GL_RGBA16F, platform_screen_width, platform_screen_height);
+        glNamedFramebufferRenderbuffer(opengl_main_fbo, GL_COLOR_ATTACHMENT0,
+            GL_RENDERBUFFER, opengl_main_fbo_color0);
+
+        glNamedRenderbufferStorageMultisample(opengl_main_fbo_depth, fbo_samples,
+            GL_DEPTH_COMPONENT32F, platform_screen_width, platform_screen_height);
+        glNamedFramebufferRenderbuffer(opengl_main_fbo, GL_DEPTH_ATTACHMENT,
+            GL_RENDERBUFFER, opengl_main_fbo_depth);
+    }
 }
 
 static void opengl_present(void) {
-    glClearColor(0.6f, 0.2f, 0.2f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    float32 clear_color0[] = {0.6f, 0.2f, 0.2f, 1.0f};
+    glClearNamedFramebufferfv(opengl_main_fbo, GL_COLOR, 0, clear_color0);
+    float32 clear_depth = 0.0f;
+    glClearNamedFramebufferfv(opengl_main_fbo, GL_DEPTH, 0, &clear_depth);
+    glBindFramebuffer(GL_FRAMEBUFFER, opengl_main_fbo);
+
+    // note(dfra): fixes intel default framebuffer resize bug
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glClear(0);
+
+    glEnable(GL_FRAMEBUFFER_SRGB);
+    glBlitNamedFramebuffer(opengl_main_fbo, 0,
+        0, 0, platform_screen_width, platform_screen_height,
+        0, 0, platform_screen_width, platform_screen_height,
+        GL_COLOR_BUFFER_BIT, GL_NEAREST);
+    glDisable(GL_FRAMEBUFFER_SRGB);
+
     opengl_platform_present();
 }
