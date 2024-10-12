73322ba00d973721944cbae9cb715bad4a331c04
Dawson Frakes <dawsonmfrakes@gmail.com>
2024-10-12 17:25:43 -0400

    clear screen, again, again, again

--- game.d
+++ game.d
@@ -1,5 +1,8 @@
 #!dmd -betterC -run game
 
+enum DEVELOPER = true;
+version = OpenGL;
+
 version (Windows) {
 	// kernel32
 	struct HINSTANCE__;
@@ -8,6 +11,7 @@ version (Windows) {
 	alias PROC = extern(Windows) ptrdiff_t function();
 
 	extern extern(Windows) HMODULE GetModuleHandleW(const(wchar)*);
+	extern extern(Windows) void Sleep(uint);
 	extern extern(Windows) noreturn ExitProcess(uint);
 
 	// user32
@@ -36,6 +40,13 @@ version (Windows) {
 	enum WM_SYSKEYUP = 0x0105;
 	enum WM_SYSCOMMAND = 0x0112;
 	enum SC_KEYMENU = 0xF100;
+	enum GWL_STYLE = -16;
+	enum HWND_TOP = cast(HWND) 0;
+	enum SWP_NOSIZE = 0x0001;
+	enum SWP_NOMOVE = 0x0002;
+	enum SWP_NOZORDER = 0x0004;
+	enum SWP_FRAMECHANGED = 0x0020;
+	enum MONITOR_DEFAULTTOPRIMARY = 0x00000001;
 	enum VK_RETURN = 0x0D;
 	enum VK_ESCAPE = 0x1B;
 	enum VK_F4 = 0x73;
@@ -90,6 +101,21 @@ version (Windows) {
 		POINT pt;
 		uint lPrivate;
 	}
+	struct WINDOWPLACEMENT {
+		uint length;
+		uint flags;
+		uint showCmd;
+		POINT ptMinPosition;
+		POINT ptMaxPosition;
+		RECT rcNormalPosition;
+		RECT rcDevice;
+	}
+	struct MONITORINFO {
+		uint cbSize;
+		RECT rcMonitor;
+		RECT rcWork;
+		uint dwFlags;
+	}
 
 	extern extern(Windows) int SetProcessDPIAware();
 	extern extern(Windows) HICON LoadIconW(HINSTANCE, const(wchar)*);
@@ -104,6 +130,61 @@ version (Windows) {
 	extern extern(Windows) int DestroyWindow(HWND);
 	extern extern(Windows) HDC GetDC(HWND);
 	extern extern(Windows) int ValidateRect(HWND, const(RECT)*);
+	extern extern(Windows) long GetWindowLongPtrW(HWND, int);
+	extern extern(Windows) long SetWindowLongPtrW(HWND, int, long);
+	extern extern(Windows) int GetWindowPlacement(HWND, WINDOWPLACEMENT*);
+	extern extern(Windows) int SetWindowPlacement(HWND, const(WINDOWPLACEMENT)*);
+	extern extern(Windows) int SetWindowPos(HWND, HWND, int, int, int, int, uint);
+	extern extern(Windows) HMONITOR MonitorFromWindow(HWND, uint);
+	extern extern(Windows) int GetMonitorInfoW(HMONITOR, MONITORINFO*);
+
+	// gdi32
+	enum PFD_DOUBLEBUFFER = 0x00000001;
+	enum PFD_DRAW_TO_WINDOW = 0x00000004;
+	enum PFD_SUPPORT_OPENGL = 0x00000020;
+	enum PFD_DEPTH_DONTCARE = 0x20000000;
+
+	struct PIXELFORMATDESCRIPTOR {
+		ushort nSize;
+		ushort nVersion;
+		uint dwFlags;
+		ubyte iPixelType;
+		ubyte cColorBits;
+		ubyte cRedBits;
+		ubyte cRedShift;
+		ubyte cGreenBits;
+		ubyte cGreenShift;
+		ubyte cBlueBits;
+		ubyte cBlueShift;
+		ubyte cAlphaBits;
+		ubyte cAlphaShift;
+		ubyte cAccumBits;
+		ubyte cAccumRedBits;
+		ubyte cAccumGreenBits;
+		ubyte cAccumBlueBits;
+		ubyte cAccumAlphaBits;
+		ubyte cDepthBits;
+		ubyte cStencilBits;
+		ubyte cAuxBuffers;
+		ubyte iLayerType;
+		ubyte bReserved;
+		uint dwLayerMask;
+		uint dwVisibleMask;
+		uint dwDamageMask;
+	}
+
+	extern extern(Windows) int ChoosePixelFormat(HDC, const(PIXELFORMATDESCRIPTOR)*);
+	extern extern(Windows) int SetPixelFormat(HDC, int, const(PIXELFORMATDESCRIPTOR)*);
+	extern extern(Windows) int SwapBuffers(HDC);
+
+	// opengl32
+	struct HGLRC__;
+	alias HGLRC = HGLRC__*;
+
+	extern extern(Windows) HGLRC wglCreateContext(HDC);
+	extern extern(Windows) int wglDeleteContext(HGLRC);
+	extern extern(Windows) int wglMakeCurrent(HDC, HGLRC);
+	extern extern(Windows) PROC wglGetProcAddress(const(char)*);
 
 	// dwmapi
 	enum DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
@@ -113,18 +194,126 @@ version (Windows) {
 	extern extern(Windows) int DwmSetWindowAttribute(HWND, uint, const(void)*, uint);
 
 	// winmm
+	enum TIMERR_NOERROR = 0;
+
 	extern extern(Windows) uint timeBeginPeriod(uint);
 }
 
+version (OpenGL) {
+	// 1.0
+	enum GL_COLOR_BUFFER_BIT = 0x00004000;
+	enum GL_UNSIGNED_BYTE = 0x1401;
+	enum GL_FLOAT = 0x1406;
+
+	extern extern(System) void glEnable(uint);
+	extern extern(System) void glDisable(uint);
+	extern extern(System) void glViewport(int, int, uint, uint);
+	extern extern(System) void glClearColor(float, float, float, float);
+	extern extern(System) void glClear(uint);
+
+	// 3.0
+	enum GL_FRAMEBUFFER_SRGB = 0x8DB9;
+}
+
+version (OpenGL) {
+	version (Windows) {
+		enum WGL_CONTEXT_MAJOR_VERSION_ARB = 0x2091;
+		enum WGL_CONTEXT_MINOR_VERSION_ARB = 0x2092;
+		enum WGL_CONTEXT_FLAGS_ARB = 0x2094;
+		enum WGL_CONTEXT_PROFILE_MASK_ARB = 0x9126;
+		enum WGL_CONTEXT_CORE_PROFILE_BIT_ARB = 0x00000001;
+		enum WGL_CONTEXT_DEBUG_BIT_ARB = 0x0001;
+
+		__gshared HGLRC opengl_ctx = null;
+
+		void opengl_platform_init() {
+			PIXELFORMATDESCRIPTOR pfd;
+			pfd.nSize = PIXELFORMATDESCRIPTOR.sizeof;
+			pfd.nVersion = 1;
+			pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER | PFD_DEPTH_DONTCARE;
+			pfd.cColorBits = 24;
+			int format = ChoosePixelFormat(platform_hdc, &pfd);
+			SetPixelFormat(platform_hdc, format, &pfd);
+
+			HGLRC temp_ctx = wglCreateContext(platform_hdc);
+			scope(exit) wglDeleteContext(temp_ctx);
+			wglMakeCurrent(platform_hdc, temp_ctx);
+
+			extern(Windows) HGLRC function(HDC, HGLRC, const(int)*) wglCreateContextAttribsARB =
+				cast(HGLRC function(HDC, HGLRC, const(int)*))
+				wglGetProcAddress("wglCreateContextAttribsARB");
+
+			int[9] attribs = [
+				WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+				WGL_CONTEXT_MINOR_VERSION_ARB, 6,
+				WGL_CONTEXT_FLAGS_ARB, DEVELOPER ? WGL_CONTEXT_DEBUG_BIT_ARB : 0,
+				WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+				0,
+			];
+			opengl_ctx = wglCreateContextAttribsARB(platform_hdc, null, attribs.ptr);
+			wglMakeCurrent(platform_hdc, opengl_ctx);
+		}
+
+		void opengl_platform_deinit() {
+			if (opengl_ctx) wglDeleteContext(opengl_ctx);
+			opengl_ctx = null;
+		}
+
+		void opengl_platform_present() {
+			SwapBuffers(platform_hdc);
+		}
+	}
+
+	void opengl_init() {
+		opengl_platform_init();
+	}
+
+	void opengl_deinit() {
+		opengl_platform_deinit();
+	}
+
+	void opengl_resize() {
+
+	}
+
+	void opengl_present() {
+		glEnable(GL_FRAMEBUFFER_SRGB);
+		glViewport(0, 0, platform_screen_width, platform_screen_height);
+		glClearColor(0.6, 0.2, 0.2, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		opengl_platform_present();
+	}
+}
+
 version (Windows) {
-	__gshared ushort platform_screen_width;
-	__gshared ushort platform_screen_height;
-	__gshared HINSTANCE platform_hinstance;
-	__gshared HWND platform_hwnd;
-	__gshared HDC platform_hdc;
+	__gshared ushort platform_screen_width = void;
+	__gshared ushort platform_screen_height = void;
+	__gshared HINSTANCE platform_hinstance = void;
+	__gshared HWND platform_hwnd = void;
+	__gshared HDC platform_hdc = void;
 
 	void toggle_fullscreen() {
+		__gshared WINDOWPLACEMENT save_placement = {WINDOWPLACEMENT.sizeof};
 
+		uint style = cast(uint) GetWindowLongPtrW(platform_hwnd, GWL_STYLE);
+		if (style & WS_OVERLAPPEDWINDOW) {
+			MONITORINFO mi = {MONITORINFO.sizeof};
+			GetMonitorInfoW(MonitorFromWindow(platform_hwnd, MONITOR_DEFAULTTOPRIMARY), &mi);
+
+			GetWindowPlacement(platform_hwnd, &save_placement);
+			SetWindowLongPtrW(platform_hwnd, GWL_STYLE, style & ~cast(uint) WS_OVERLAPPEDWINDOW);
+			SetWindowPos(platform_hwnd, HWND_TOP,
+				mi.rcMonitor.left, mi.rcMonitor.top,
+				mi.rcMonitor.right - mi.rcMonitor.left,
+				mi.rcMonitor.bottom - mi.rcMonitor.top,
+				SWP_FRAMECHANGED);
+		} else {
+			SetWindowLongPtrW(platform_hwnd, GWL_STYLE, style | WS_OVERLAPPEDWINDOW);
+			SetWindowPlacement(platform_hwnd, &save_placement);
+			SetWindowPos(platform_hwnd, null, 0, 0, 0, 0, SWP_NOSIZE |
+				SWP_NOMOVE | SWP_NOZORDER | SWP_FRAMECHANGED);
+		}
 	}
 
 	void update_cursor_clip() {
@@ -137,6 +326,8 @@ version (Windows) {
 
 	extern(Windows) noreturn WinMainCRTStartup() {
 		platform_hinstance = GetModuleHandleW(null);
+
+		bool sleep_is_granular = timeBeginPeriod(1) == TIMERR_NOERROR;
 
 		SetProcessDPIAware();
 		WNDCLASSEXW wndclass;
@@ -156,6 +347,8 @@ version (Windows) {
 				case WM_SIZE:
 					platform_screen_width = cast(ushort) lParam;
 					platform_screen_height = cast(ushort) (cast(size_t) lParam >> 16);
+
+					opengl_resize();
 					return 0;
 				case WM_CREATE:
 					platform_hwnd = hwnd;
@@ -165,8 +358,12 @@ version (Windows) {
 					DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark_mode, dark_mode.sizeof);
 					int round_mode = DWMWCP_DONOTROUND;
 					DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, &round_mode, round_mode.sizeof);
+
+					opengl_init();
 					return 0;
 				case WM_DESTROY:
+					opengl_deinit();
+
 					PostQuitMessage(0);
 					return 0;
 				case WM_SYSCOMMAND:
@@ -214,6 +411,12 @@ version (Windows) {
 						DispatchMessageW(&msg);
 				}
 			}
+
+			opengl_present();
+
+			if (sleep_is_granular) {
+				Sleep(1);
+			}
 		}
 
 		ExitProcess(0);
@@ -224,6 +427,8 @@ version (Windows) {
 	pragma(linkerDirective, "-subsystem:windows");
 	pragma(lib, "kernel32");
 	pragma(lib, "user32");
+	pragma(lib, "gdi32");
+	pragma(lib, "opengl32");
 	pragma(lib, "dwmapi");
 	pragma(lib, "winmm");
 }
