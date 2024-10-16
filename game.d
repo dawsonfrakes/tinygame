fe9784df44e30a3c19ac0a1337cfbd11476583aa
Dawson Frakes <dawsonmfrakes@gmail.com>
2024-10-16 12:37:09 -0400

    clear screen in d

--- game.d
+++ game.d
@@ -10,181 +10,300 @@ template Procedure(RT, string name_, Args...) {
     enum Procedure = Internal();
 }
 
-version(Windows) {
-    // kernel32
-    struct HINSTANCE__; alias HINSTANCE = HINSTANCE__*;
-    alias HMODULE = HINSTANCE;
-    alias PROC = extern(Windows) ptrdiff_t function();
+// kernel32
+struct HINSTANCE__; alias HINSTANCE = HINSTANCE__*;
+alias HMODULE = HINSTANCE;
+alias PROC = extern(Windows) ptrdiff_t function();
 
-    alias kernel32 = AliasSeq!(
-        Procedure!(HMODULE, "GetModuleHandleW", const(wchar)*),
-        Procedure!(HMODULE, "LoadLibraryW", const(wchar)*),
-        Procedure!(PROC, "GetProcAddress", HMODULE, const(char)*),
-        Procedure!(void, "Sleep", uint),
-        Procedure!(noreturn, "ExitProcess", uint),
-    );
+alias kernel32 = AliasSeq!(
+    Procedure!(HMODULE, "GetModuleHandleW", const(wchar)*),
+    Procedure!(HMODULE, "LoadLibraryW", const(wchar)*),
+    Procedure!(PROC, "GetProcAddress", HMODULE, const(char)*),
+    Procedure!(void, "Sleep", uint),
+    Procedure!(noreturn, "ExitProcess", uint),
+);
 
-    // user32
-    enum IDI_WARNING = cast(const(wchar)*) 32515;
-    enum IDC_CROSS = cast(const(wchar)*) 32515;
-    enum CS_OWNDC = 0x0020;
-    enum WS_MAXIMIZEBOX = 0x00010000;
-    enum WS_MINIMIZEBOX = 0x00020000;
-    enum WS_THICKFRAME = 0x00040000;
-    enum WS_SYSMENU = 0x00080000;
-    enum WS_CAPTION = 0x00C00000;
-    enum WS_VISIBLE = 0x10000000;
-    enum WS_OVERLAPPEDWINDOW = WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
-    enum CW_USEDEFAULT = 0x80000000;
-    enum PM_REMOVE = 0x0001;
-    enum WM_CREATE = 0x0001;
-    enum WM_DESTROY = 0x0002;
-    enum WM_SIZE = 0x0005;
-    enum WM_PAINT = 0x000F;
-    enum WM_QUIT = 0x0012;
-    enum WM_ERASEBKGND = 0x0014;
-    enum WM_ACTIVATEAPP = 0x001C;
-    enum WM_KEYDOWN = 0x0100;
-    enum WM_KEYUP = 0x0101;
-    enum WM_SYSKEYDOWN = 0x0104;
-    enum WM_SYSKEYUP = 0x0105;
-    enum WM_SYSCOMMAND = 0x0112;
-    enum SC_KEYMENU = 0xF100;
-    enum GWL_STYLE = -16;
-    enum HWND_TOP = cast(HWND) 0;
-    enum SWP_NOSIZE = 0x0001;
-    enum SWP_NOMOVE = 0x0002;
-    enum SWP_NOZORDER = 0x0004;
-    enum SWP_FRAMECHANGED = 0x0020;
-    enum MONITOR_DEFAULTTOPRIMARY = 0x00000001;
-    enum VK_RETURN = 0x0D;
-    enum VK_ESCAPE = 0x1B;
-    enum VK_F4 = 0x73;
-    enum VK_F10 = 0x79;
-    enum VK_F11 = 0x7A;
+// user32
+enum IDI_WARNING = cast(const(wchar)*) 32515;
+enum IDC_CROSS = cast(const(wchar)*) 32515;
+enum CS_OWNDC = 0x0020;
+enum WS_MAXIMIZEBOX = 0x00010000;
+enum WS_MINIMIZEBOX = 0x00020000;
+enum WS_THICKFRAME = 0x00040000;
+enum WS_SYSMENU = 0x00080000;
+enum WS_CAPTION = 0x00C00000;
+enum WS_VISIBLE = 0x10000000;
+enum WS_OVERLAPPEDWINDOW = WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+enum CW_USEDEFAULT = 0x80000000;
+enum PM_REMOVE = 0x0001;
+enum WM_CREATE = 0x0001;
+enum WM_DESTROY = 0x0002;
+enum WM_SIZE = 0x0005;
+enum WM_PAINT = 0x000F;
+enum WM_QUIT = 0x0012;
+enum WM_ERASEBKGND = 0x0014;
+enum WM_ACTIVATEAPP = 0x001C;
+enum WM_KEYDOWN = 0x0100;
+enum WM_KEYUP = 0x0101;
+enum WM_SYSKEYDOWN = 0x0104;
+enum WM_SYSKEYUP = 0x0105;
+enum WM_SYSCOMMAND = 0x0112;
+enum SC_KEYMENU = 0xF100;
+enum GWL_STYLE = -16;
+enum HWND_TOP = cast(HWND) 0;
+enum SWP_NOSIZE = 0x0001;
+enum SWP_NOMOVE = 0x0002;
+enum SWP_NOZORDER = 0x0004;
+enum SWP_FRAMECHANGED = 0x0020;
+enum MONITOR_DEFAULTTOPRIMARY = 0x00000001;
+enum VK_RETURN = 0x0D;
+enum VK_ESCAPE = 0x1B;
+enum VK_F4 = 0x73;
+enum VK_F10 = 0x79;
+enum VK_F11 = 0x7A;
 
-    struct HDC__; alias HDC = HDC__*;
-    struct HWND__; alias HWND = HWND__*;
-    struct HMENU__; alias HMENU = HMENU__*;
-    struct HICON__; alias HICON = HICON__*;
-    struct HBRUSH__; alias HBRUSH = HBRUSH__*;
-    struct HCURSOR__; alias HCURSOR = HCURSOR__*;
-    struct HMONITOR__; alias HMONITOR = HMONITOR__*;
-    alias WNDPROC = extern(Windows) ptrdiff_t function(HWND, uint, size_t, ptrdiff_t);
-    struct POINT {
-        int x;
-        int y;
-    }
-    struct RECT {
-        int left;
-        int top;
-        int right;
-        int bottom;
-    }
-    struct WNDCLASSEXW {
-        uint cbSize;
-        uint style;
-        WNDPROC lpfnWndProc;
-        int cbClsExtra;
-        int cbWndExtra;
-        HINSTANCE hInstance;
-        HICON hIcon;
-        HCURSOR hCursor;
-        HBRUSH hbrBackground;
-        const(wchar)* lpszMenuName;
-        const(wchar)* lpszClassName;
-        HICON hIconSm;
-    }
-    struct MSG {
-        HWND hwnd;
-        uint message;
-        size_t wParam;
-        ptrdiff_t lParam;
-        uint time;
-        POINT pt;
-        uint lPrivate;
-    }
-    struct WINDOWPLACEMENT {
-        uint length;
-        uint flags;
-        uint showCmd;
-        POINT ptMinPosition;
-        POINT ptMaxPosition;
-        RECT rcNormalPosition;
-        RECT rcDevice;
-    }
-    struct MONITORINFO {
-        uint cbSize;
-        RECT rcMonitor;
-        RECT rcWork;
-        uint dwFlags;
-    }
-
-    alias user32 = AliasSeq!(
-        Procedure!(int, "SetProcessDPIAware"),
-        Procedure!(HICON, "LoadIconW", HINSTANCE, const(wchar)*),
-        Procedure!(HCURSOR, "LoadCursorW", HINSTANCE, const(wchar)*),
-        Procedure!(ushort, "RegisterClassExW", const(WNDCLASSEXW)*),
-        Procedure!(HWND, "CreateWindowExW", uint, const(wchar)*, const(wchar)*, uint, int, int, int, int, HWND, HMENU, HINSTANCE, void*),
-        Procedure!(int, "PeekMessageW", MSG*, HWND, uint, uint, uint),
-        Procedure!(int, "TranslateMessage", const(MSG)*),
-        Procedure!(ptrdiff_t, "DispatchMessageW", const(MSG)*),
-        Procedure!(HDC, "GetDC", HWND),
-        Procedure!(int, "ValidateRect", HWND, const(RECT)*),
-        Procedure!(int, "DestroyWindow", HWND),
-        Procedure!(ptrdiff_t, "GetWindowLongPtrW", HWND, int),
-        Procedure!(ptrdiff_t, "SetWindowLongPtrW", HWND, int, ptrdiff_t),
-        Procedure!(int, "GetWindowPlacement", HWND, WINDOWPLACEMENT*),
-        Procedure!(int, "SetWindowPlacement", HWND, const(WINDOWPLACEMENT)*),
-        Procedure!(int, "SetWindowPos", HWND, HWND, int, int, int, int, uint),
-        Procedure!(HMONITOR, "MonitorFromWindow", HWND, uint),
-        Procedure!(int, "GetMonitorInfoW", HMONITOR, MONITORINFO*),
-        Procedure!(ptrdiff_t, "DefWindowProcW", HWND, uint, size_t, ptrdiff_t),
-        Procedure!(void, "PostQuitMessage", int),
-    );
-
-    // dwmapi
-    enum DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
-    enum DWMWA_WINDOW_CORNER_PREFERENCE = 33;
-    enum DWMWCP_DONOTROUND = 1;
-
-    alias dwmapi = AliasSeq!(
-        Procedure!(int, "DwmSetWindowAttribute", HWND, uint, const(void)*, uint),
-    );
-
-    // winmm
-    enum TIMERR_NOERROR = 0;
-
-    alias winmm = AliasSeq!(
-        Procedure!(uint, "timeBeginPeriod", uint),
-    );
+struct HDC__; alias HDC = HDC__*;
+struct HWND__; alias HWND = HWND__*;
+struct HMENU__; alias HMENU = HMENU__*;
+struct HICON__; alias HICON = HICON__*;
+struct HBRUSH__; alias HBRUSH = HBRUSH__*;
+struct HCURSOR__; alias HCURSOR = HCURSOR__*;
+struct HMONITOR__; alias HMONITOR = HMONITOR__*;
+alias WNDPROC = extern(Windows) ptrdiff_t function(HWND, uint, size_t, ptrdiff_t);
+struct POINT {
+    int x;
+    int y;
+}
+struct RECT {
+    int left;
+    int top;
+    int right;
+    int bottom;
+}
+struct WNDCLASSEXW {
+    uint cbSize;
+    uint style;
+    WNDPROC lpfnWndProc;
+    int cbClsExtra;
+    int cbWndExtra;
+    HINSTANCE hInstance;
+    HICON hIcon;
+    HCURSOR hCursor;
+    HBRUSH hbrBackground;
+    const(wchar)* lpszMenuName;
+    const(wchar)* lpszClassName;
+    HICON hIconSm;
+}
+struct MSG {
+    HWND hwnd;
+    uint message;
+    size_t wParam;
+    ptrdiff_t lParam;
+    uint time;
+    POINT pt;
+    uint lPrivate;
+}
+struct WINDOWPLACEMENT {
+    uint length;
+    uint flags;
+    uint showCmd;
+    POINT ptMinPosition;
+    POINT ptMaxPosition;
+    RECT rcNormalPosition;
+    RECT rcDevice;
+}
+struct MONITORINFO {
+    uint cbSize;
+    RECT rcMonitor;
+    RECT rcWork;
+    uint dwFlags;
 }
 
-version = Steam;
-version (Steam) {
-    enum SteamAPIInitResult {
-        ok,
-        failed_generic,
-        no_steam_client,
-        version_mismatch,
+alias user32 = AliasSeq!(
+    Procedure!(int, "SetProcessDPIAware"),
+    Procedure!(HICON, "LoadIconW", HINSTANCE, const(wchar)*),
+    Procedure!(HCURSOR, "LoadCursorW", HINSTANCE, const(wchar)*),
+    Procedure!(ushort, "RegisterClassExW", const(WNDCLASSEXW)*),
+    Procedure!(HWND, "CreateWindowExW", uint, const(wchar)*, const(wchar)*, uint, int, int, int, int, HWND, HMENU, HINSTANCE, void*),
+    Procedure!(int, "PeekMessageW", MSG*, HWND, uint, uint, uint),
+    Procedure!(int, "TranslateMessage", const(MSG)*),
+    Procedure!(ptrdiff_t, "DispatchMessageW", const(MSG)*),
+    Procedure!(HDC, "GetDC", HWND),
+    Procedure!(int, "ValidateRect", HWND, const(RECT)*),
+    Procedure!(int, "DestroyWindow", HWND),
+    Procedure!(ptrdiff_t, "GetWindowLongPtrW", HWND, int),
+    Procedure!(ptrdiff_t, "SetWindowLongPtrW", HWND, int, ptrdiff_t),
+    Procedure!(int, "GetWindowPlacement", HWND, WINDOWPLACEMENT*),
+    Procedure!(int, "SetWindowPlacement", HWND, const(WINDOWPLACEMENT)*),
+    Procedure!(int, "SetWindowPos", HWND, HWND, int, int, int, int, uint),
+    Procedure!(HMONITOR, "MonitorFromWindow", HWND, uint),
+    Procedure!(int, "GetMonitorInfoW", HMONITOR, MONITORINFO*),
+    Procedure!(ptrdiff_t, "DefWindowProcW", HWND, uint, size_t, ptrdiff_t),
+    Procedure!(void, "PostQuitMessage", int),
+);
+
+// gdi32
+enum PFD_DOUBLEBUFFER = 0x00000001;
+enum PFD_DRAW_TO_WINDOW = 0x00000004;
+enum PFD_SUPPORT_OPENGL = 0x00000020;
+enum PFD_DEPTH_DONTCARE = 0x20000000;
+
+struct PIXELFORMATDESCRIPTOR {
+    ushort nSize;
+    ushort nVersion;
+    uint dwFlags;
+    byte iPixelType;
+    byte cColorBits;
+    byte cRedBits;
+    byte cRedShift;
+    byte cGreenBits;
+    byte cGreenShift;
+    byte cBlueBits;
+    byte cBlueShift;
+    byte cAlphaBits;
+    byte cAlphaShift;
+    byte cAccumBits;
+    byte cAccumRedBits;
+    byte cAccumGreenBits;
+    byte cAccumBlueBits;
+    byte cAccumAlphaBits;
+    byte cDepthBits;
+    byte cStencilBits;
+    byte cAuxBuffers;
+    byte iLayerType;
+    byte bReserved;
+    uint dwLayerMask;
+    uint dwVisibleMask;
+    uint dwDamageMask;
+}
+
+alias gdi32 = AliasSeq!(
+    Procedure!(int, "ChoosePixelFormat", HDC, const(PIXELFORMATDESCRIPTOR)*),
+    Procedure!(int, "SetPixelFormat", HDC, int, const(PIXELFORMATDESCRIPTOR)*),
+    Procedure!(int, "SwapBuffers", HDC),
+);
+
+// opengl32
+struct HGLRC__; alias HGLRC = HGLRC__*;
+
+alias opengl32 = AliasSeq!(
+    Procedure!(HGLRC, "wglCreateContext", HDC),
+    Procedure!(int, "wglDeleteContext", HGLRC),
+    Procedure!(int, "wglMakeCurrent", HDC, HGLRC),
+    Procedure!(PROC, "wglGetProcAddress", const(char)*),
+);
+
+// dwmapi
+enum DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+enum DWMWA_WINDOW_CORNER_PREFERENCE = 33;
+enum DWMWCP_DONOTROUND = 1;
+
+alias dwmapi = AliasSeq!(
+    Procedure!(int, "DwmSetWindowAttribute", HWND, uint, const(void)*, uint),
+);
+
+// winmm
+enum TIMERR_NOERROR = 0;
+
+alias winmm = AliasSeq!(
+    Procedure!(uint, "timeBeginPeriod", uint),
+);
+
+// steam_api64
+enum SteamAPIInitResult {
+    ok,
+    failed_generic,
+    no_steam_client,
+    version_mismatch,
+}
+
+alias steam_api64 = AliasSeq!(
+    Procedure!(SteamAPIInitResult, "SteamAPI_InitFlat", const(char)[1024]*),
+    Procedure!(void, "SteamAPI_RunCallbacks"),
+    Procedure!(void, "SteamAPI_Shutdown"),
+);
+
+// gl10
+enum GL_COLOR_BUFFER_BIT = 0x00004000;
+alias gl10 = AliasSeq!(
+    Procedure!(void, "glEnable", uint),
+    Procedure!(void, "glDisable", uint),
+    Procedure!(void, "glViewport", int, int, uint, uint),
+    Procedure!(void, "glClearColor", float, float, float, float),
+    Procedure!(void, "glClear", uint),
+);
+
+// gl30
+enum GL_FRAMEBUFFER_SRGB = 0x8DB9;
+
+struct OpenGLRenderer {
+    version (Windows) {
+        static foreach (proc; gl10) {
+            mixin("alias ReturnType_" ~ proc.name ~ " = " ~ proc.ReturnType.stringof ~ ";");
+            mixin("__gshared extern(C) ReturnType_" ~ proc.name ~ " function" ~ proc.ArgTypes.stringof ~ " " ~ proc.name ~ ";");
+        }
+
+        static void platform_init() {
+            HMODULE dll = LoadLibraryW("opengl32"w.ptr);
+            static foreach (proc; gl10) {
+                mixin(proc.name ~ " = cast(typeof(" ~ proc.name ~ ")) GetProcAddress(dll, \"" ~ proc.name ~ "\");");
+            }
+
+            PIXELFORMATDESCRIPTOR pfd;
+            pfd.nSize = PIXELFORMATDESCRIPTOR.sizeof;
+            pfd.nVersion = 1;
+            pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL |
+                PFD_DOUBLEBUFFER | PFD_DEPTH_DONTCARE;
+            pfd.cColorBits = 24;
+            int format = ChoosePixelFormat(platform_hdc, &pfd);
+            SetPixelFormat(platform_hdc, format, &pfd);
+
+            HGLRC temp_ctx = wglCreateContext(platform_hdc);
+            wglMakeCurrent(platform_hdc, temp_ctx);
+        }
+
+        static void platform_deinit() {
+
+        }
+
+        static void platform_present() {
+            SwapBuffers(platform_hdc);
+        }
     }
 
-    alias steam_api64 = AliasSeq!(
-        Procedure!(SteamAPIInitResult, "SteamAPI_InitFlat", const(char)[1024]*),
-        Procedure!(void, "SteamAPI_RunCallbacks"),
-        Procedure!(void, "SteamAPI_Shutdown"),
-    );
+    static void init() {
+        platform_init();
+
+        glEnable(GL_FRAMEBUFFER_SRGB);
+    }
+
+    static void deinit() {
+        platform_deinit();
+    }
+
+    static void resize() {
+
+    }
+
+    static void present() {
+        glViewport(0, 0, platform_screen_width, platform_screen_height);
+        glClearColor(0.6, 0.2, 0.2, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        platform_present();
+    }
 }
 
 version (Windows) {
     enum DEVELOPER = true;
+    alias Renderer = OpenGLRenderer;
 
     static foreach (proc; kernel32) {
         mixin("alias ReturnType_" ~ proc.name ~ " = " ~ proc.ReturnType.stringof ~ ";");
         mixin("extern extern(Windows) ReturnType_" ~ proc.name ~ " " ~ proc.name ~ proc.ArgTypes.stringof ~ ";");
     }
 
-    enum dynamic_procedure_modules = ["user32", "dwmapi", "winmm"];
+    enum dynamic_procedure_modules = ["user32", "gdi32", "opengl32", "dwmapi", "winmm"];
     static foreach (lib; dynamic_procedure_modules) {
         static foreach (proc; mixin(lib)) {
             mixin("alias ReturnType_" ~ proc.name ~ " = " ~ proc.ReturnType.stringof ~ ";");
@@ -248,7 +367,7 @@ version (Windows) {
 
         platform_hinstance = GetModuleHandleW(null);
 
-        bool sleep_is_granular = timeBeginPeriod(1) == TIMERR_NOERROR;
+        bool sleep_is_granular = timeBeginPeriod && timeBeginPeriod(1) == TIMERR_NOERROR;
 
         SetProcessDPIAware();
         WNDCLASSEXW wndclass;
@@ -271,17 +390,25 @@ version (Windows) {
                 case WM_SIZE:
                     platform_screen_width = cast(ushort) cast(size_t) lParam;
                     platform_screen_height = cast(ushort) (cast(size_t) lParam >> 16);
+
+                    Renderer.resize();
                     return 0;
                 case WM_CREATE:
                     platform_hwnd = hwnd;
                     platform_hdc = GetDC(hwnd);
 
-                    int dark_mode = true;
-                    DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark_mode, dark_mode.sizeof);
-                    int round_mode = DWMWCP_DONOTROUND;
-                    DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, &round_mode, round_mode.sizeof);
+                    if (DwmSetWindowAttribute) {
+                        int dark_mode = true;
+                        DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &dark_mode, dark_mode.sizeof);
+                        int round_mode = DWMWCP_DONOTROUND;
+                        DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, &round_mode, round_mode.sizeof);
+                    }
+
+                    Renderer.init();
                     return 0;
                 case WM_DESTROY:
+                    Renderer.deinit();
+
                     PostQuitMessage(0);
                     return 0;
                 default:
@@ -319,7 +446,7 @@ version (Windows) {
                                 if (DEVELOPER && wParam == VK_ESCAPE) DestroyWindow(platform_hwnd);
                             }
                         }
-                        break;
+                        goto default;
                     case WM_QUIT:
                         break game_loop;
                     default:
@@ -329,6 +456,8 @@ version (Windows) {
 
             if (platform_steam_enabled) SteamAPI_RunCallbacks();
 
+            Renderer.present();
+
             if (sleep_is_granular) {
                 Sleep(1);
             }
@@ -337,6 +466,8 @@ version (Windows) {
         if (platform_steam_enabled) SteamAPI_Shutdown();
         ExitProcess(0);
     }
+
+    extern(C) int _fltused = void;
 
     pragma(linkerDirective, "-subsystem:windows");
     pragma(lib, "kernel32");
